69963070815132c75b452470ccf702e062ef3f20
Felipe <99846344+Felipe-Euzebio@users.noreply.github.com>
2024-04-24 22:24:17 -0300

    Logging Users In

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -27,29 +27,34 @@ class _AuthScreenState extends State<AuthScreen> {
 
     _form.currentState!.save();
 
-    if(_isLogin) {
-      // Log user in
-    } else {
-      try {
+    try {
+      if(_isLogin) {
+        final userCredentials = await _firebase.signInWithEmailAndPassword(
+          email: _enteredEmail,
+          password: _enteredPassword,
+        );
+
+        print(userCredentials);
+      } else {
         final userCredentials = await _firebase.createUserWithEmailAndPassword(
           email: _enteredEmail,
           password: _enteredPassword,
         );
 
         print(userCredentials);
-      } on FirebaseAuthException catch (e) {
-        if (e.code == 'email-already-in-use') {
-          // ...
-        }
-
-        ScaffoldMessenger.of(context).clearSnackBars();
-
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text(e.message ?? 'Authentication failed.'),
-          ),
-        );
       }
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'email-already-in-use') {
+        // ...
+      }
+
+      ScaffoldMessenger.of(context).clearSnackBars();
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(e.message ?? 'Authentication failed.'),
+        ),
+      );
     }
   }
 

49e0e96bf87ec6f9b557c1e406493a17aa7bb8bd
Felipe <99846344+Felipe-Euzebio@users.noreply.github.com>
2024-04-27 20:33:38 -0300

    Managing The Selected Image In The Authentication Form

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_chat/widgets/user_image_picker.dart';
@@ -18,11 +20,13 @@ class _AuthScreenState extends State<AuthScreen> {
 
   var _enteredEmail = '';
   var _enteredPassword = '';
+  File? _selectedImage;
 
   void _submit() async {
     final isValid = _form.currentState!.validate();
 
-    if(!isValid) {
+    if(!isValid || !_isLogin && _selectedImage == null) {
+      // show error message...
       return;
     }
 
@@ -84,7 +88,12 @@ class _AuthScreenState extends State<AuthScreen> {
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
                         children: [
-                          if(!_isLogin) UserImagePicker(),
+                          if(!_isLogin) 
+                            UserImagePicker(
+                              onPickImage: (pickedImage) {
+                                _selectedImage = pickedImage;
+                              },
+                            ),
                           TextFormField(
                             decoration: const InputDecoration(
                               labelText: 'Email Address',
